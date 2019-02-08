9d51c6fda86e7efa3b153eb7941ebd20866fbeea
Fady Matar <fady.matar@gmail.com>
2019-02-08 08:05:13 -0800

    Moving things around
    Signed-off-by: Fady Matar <fady.matar@gmail.com>

--- src/main/java/io/slixes/core/Slixes.java
+++ src/main/java/io/slixes/core/Slixes.java
@@ -1,5 +1,6 @@
 package io.slixes.core;
 
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
@@ -7,6 +8,7 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
@@ -14,7 +16,7 @@ import java.util.concurrent.atomic.AtomicReference;
 public interface Slixes {
 
   static JsonObject boot(Vertx vertx, Router router, JsonObject config)
-      throws SlixesException {
+    throws SlixesException {
     AtomicReference<JsonObject> result = new AtomicReference<>();
     final List<HttpServer> serverList = HttpServerCreator.create(vertx, config);
     serverList.forEach(entry -> entry.requestHandler(router).listen(ar -> {
@@ -23,40 +25,49 @@ public interface Slixes {
       }
       if (ar.failed()) {
         JsonObject errorJson = new JsonObject()
-            .put("result", false)
-            .put("error", new JsonObject()
-                .put("message", ar.cause().getMessage()));
+          .put("result", false)
+          .put("error", new JsonObject()
+            .put("message", ar.cause().getMessage()));
         result.set(errorJson);
       }
     }));
     return result.get();
   }
 
-  static void boot(Vertx vertx, Router router, JsonObject config,
-      Handler<AsyncResult<Void>> handler) {
+  static void boot(Vertx vertx, Router router, Handler<AsyncResult<Void>> handler) {
     Future<Void> future = Future.future();
     future.setHandler(handler);
-    final List<HttpServer> stringHttpServerMap;
-    try {
-      stringHttpServerMap = HttpServerCreator.create(vertx, config);
-      if (!stringHttpServerMap.isEmpty()) {
-        final CountDownLatch latch = new CountDownLatch(stringHttpServerMap.size());
-        stringHttpServerMap.forEach(entry ->
-            entry.requestHandler(router).listen(ar -> {
-              if (ar.succeeded()) {
-                latch.countDown();
-                if (latch.getCount() == 0) {
-                  future.complete();
+    final ConfigRetriever retriever = ConfigRetriever.create(vertx);
+    retriever.getConfig(configHandler -> {
+      if (configHandler.succeeded()) {
+        try {
+          final JsonObject config = configHandler.result();
+          final List<HttpServer> stringHttpServerMap = HttpServerCreator.create(vertx, config);
+          if (!stringHttpServerMap.isEmpty()) {
+            final CountDownLatch latch = new CountDownLatch(stringHttpServerMap.size());
+            stringHttpServerMap.forEach(entry ->
+              entry.requestHandler(router).listen(ar -> {
+                if (ar.succeeded()) {
+                  latch.countDown();
+                  if (latch.getCount() == 0) {
+                    future.complete();
+                  }
+                } else {
+                  future.fail(ar.cause());
                 }
-              } else {
-                future.fail(ar.cause());
-              }
-            }));
+              }));
+          } else {
+            future.fail("Nothing to boot");
+          }
+        } catch (SlixesException e) {
+          future.fail(e.getCause());
+        }
+
       } else {
-        future.fail("Nothing to boot");
+        future.fail("Unable to retrieve configuration");
       }
-    } catch (SlixesException e) {
-      e.printStackTrace();
-    }
+    });
+
+
   }
 }
